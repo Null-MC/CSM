eec12c3495fe8bf5911a231e5816a1089629355f
NULL511 <null511@outlook.com>
2022-06-18 02:55:26 -0400

    remove PCF weighting hack

--- shaders/lib/shadows/csm_render.glsl
+++ shaders/lib/shadows/csm_render.glsl
@@ -213,13 +213,14 @@ const float tile_dist_bias_factor = 0.012288;
 
                 if (sampleCount == 1) return shadow;
 
-                #if SHADOW_FILTER == 1
-                    float f = 1.0 - max(geoNoL, 0.0);
-                    f = clamp(shadow / sampleCount - 0.8*f, 0.0, 1.0) * (1.0 + 1.0 * f);
-                    return clamp(f, 0.0, 1.0);
-                #else
-                    return expStep(shadow / sampleCount);
-                #endif
+                // #if SHADOW_FILTER == 1
+                //     float f = 1.0 - max(geoNoL, 0.0);
+                //     f = clamp(shadow / sampleCount - 0.8*f, 0.0, 1.0) * (1.0 + 1.0 * f);
+                //     return clamp(f, 0.0, 1.0);
+                // #else
+                //     return shadow / sampleCount;
+                // #endif
+                return shadow / sampleCount;
             }
         #endif
     #else
